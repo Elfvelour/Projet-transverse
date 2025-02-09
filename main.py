19d25cdc1f30597d69fdfabbad3919138c636d2f
noemi <noemie.marques@efrei.net>
2025-02-09 17:38:18 +0100

    Des trucs

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 ## Auteure : MARQUES Noémie                                          ##
 ## Déroulement et fonctionnement du jeu                              ##
 #######################################################################
+
+def run_game () : #fonction qui lance le jeu
+
+
+
+run_game()
