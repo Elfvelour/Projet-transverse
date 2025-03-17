f8b7e504997c0b44f9a01200eeddc4da5715abf1
Elfvelour <timothee.girault@efrei.net>
2025-03-17 09:50:34 +0100

    bouton para créer+début forme menu

--- main_menu.py
+++ main_menu.py
@@ -3,7 +3,7 @@ import sys
 ##############################################
 ###### Programme Python menu principal  ######
 ###### Auteur: Timothée Girault         ######
-###### Version: 1.0                     ######
+###### Version: 1.3                     ######
 ##############################################
 
 ##############################################
@@ -11,6 +11,7 @@ import sys
 
 import pygame
 from pygame import MOUSEBUTTONDOWN
+from pygame.examples.cursors import image
 
 #initialisation de pygame
 pygame.init()
@@ -25,6 +26,9 @@ largeur=1520
 ecran=pygame.display.set_mode((largeur,hauteur))
 jaune=(255,255,0)
 police=36
+logo_para=pygame.transform.scale(pygame.image.load("assests/logo_parametre.png"),(50,50))
+fond_ecran=pygame.transform.scale(pygame.image.load("assests/background3.png"),(1520,790))
+action_bouton1=True
 
 ##############################################
 
@@ -36,7 +40,7 @@ class Menu:
         self.ecran=pygame.display.set_mode((largeur,hauteur))
         #donne le nom a la page
         pygame.display.set_caption("BOW MASTER")
-        #liste de bouttons
+        #liste de boutons
         self.boutons=[]
         #le jeux a commencé ou non
         self.en_train_de_jouer=True
@@ -49,11 +53,13 @@ class Menu:
 
 #classe des boutons
 class Bouton:
-    def __init__(self, texte, x, y, couleur, police,action):
+    def __init__(self, texte, x, y, couleur,hauteur,longueur,police,action):
         self.texte = texte
         self.axe_x = x
         self.axe_y = y
         self.couleur = couleur
+        self.hauteur = hauteur
+        self.longueur = longueur
         self.action = action
         self.police_caractere = pygame.font.Font(None, police)
 
@@ -63,17 +69,18 @@ class Bouton:
         texte_surface = self.police_caractere.render(self.texte, True, 'white')  # Couleur du texte : blanc
 
         # Créer un rectangle pour le bouton
-        bouton_creer = pygame.Rect(self.axe_x, self.axe_y,200,50)
+        bouton_creer = pygame.Rect(self.axe_x, self.axe_y,self.hauteur,self.longueur)
 
         # Dessiner le rectangle sur l'écran avec la couleur spécifiée
         pygame.draw.rect(ecran, self.couleur, bouton_creer)
         texte_rectangle = texte_surface.get_rect(center=bouton_creer.center)
 
         #animation du bouton
-        if self.BoutonClique():
-            pygame.draw.rect(ecran,'dark red', bouton_creer,0,5)
-        else:
-            pygame.draw.rect(ecran, 'red', bouton_creer, 0, 5)
+        if self.couleur == 'red':
+            if self.BoutonClique():
+                pygame.draw.rect(ecran,'dark red', bouton_creer,0,5)
+            else:
+                pygame.draw.rect(ecran, 'red', bouton_creer, 0, 5)
         ecran.blit(texte_surface, texte_rectangle)
 
 
@@ -87,33 +94,40 @@ class Bouton:
             return False
 
 
-
 menu=Menu()
 
 # Définir les paramètres du bouton
 texte = "jouer"
 x = 650
-y = 310
+y = 420
 couleur = 'red'  # Rouge
 police = 36
+hauteur = 200
+longueur = 50
 
 #bouton jouer
-mon_bouton_jouer = Bouton(texte, x, y, couleur, police,True)
+mon_bouton_jouer = Bouton(texte, x, y, couleur,hauteur,longueur, police,action_bouton1)
 #bouton quitter
-mon_bouton_quitter=Bouton("quitter",650,390,'red',36,True)
+mon_bouton_quitter=Bouton("quitter",650,500,'red',200,50,36,True)
+#bouton parametre
+mon_bouton_parametre=Bouton("",1470,730,'white',50,50,36,True)
 
 # Boucle principale
 running = True
 while running:
     # Remplir l'écran avec une couleur de fond
     ecran.fill('white')  # blanc
+    mon_bouton_parametre.CreationBouton(ecran)
+    ecran.blit(fond_ecran,(0,0))
 
     # Créer et affiche les boutons
     mon_bouton_jouer.CreationBouton(ecran)
     mon_bouton_quitter.CreationBouton(ecran)
+    ecran.blit(logo_para, (1470,730))
     print(mon_bouton_jouer.BoutonClique())
     # Mettre à jour l'affichage
     pygame.display.flip()
+
     if mon_bouton_quitter.BoutonClique()==True:
         running = False
     for event in pygame.event.get():
