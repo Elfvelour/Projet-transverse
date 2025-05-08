0e8d9c0ed358f979d54602012ea9b133c3b337a6
Toxo687 <thomas.aubert@efrei.net>
2025-05-08 14:21:36 +0200

    Le bot tire avec son arme et pas celle choisi par le joueur.
    Correction bug arme du bot

--- jeu.py
+++ jeu.py
@@ -19,25 +19,28 @@ class Jeu:
         self.ecran = screen
         self.perso = perso
         self.arme = arme
-        self.image_projectile = self.obtenir_image_arme(self.perso, self.arme)
+        self.image_projectile_joueur = self.obtenir_image_arme(self.perso, self.arme)
+        self.image_projectile_bot = self.obtenir_image_arme("PB", "A0")  # arme par défaut du bot
         self.background = pygame.image.load("assets/images/menup/logoiajeu.jpeg")
         self.sol = Sol()
         self.joueur = Joueur(100, 670, [32, 64], perso, arme)
-        self.donnees_json = self.charger_donnees_json("gestion_stats.json")  # chargement des données
+        self.donnees_json = self.charger_donnees_json("gestion_stats.json")
         self.sons = sons
         self.projectiles_joueur = pygame.sprite.Group()
         self.projectiles_bot = pygame.sprite.Group()
 
         self.piece = Pieces((50, 50))
-        self.bot = Bot((1920 - 200), 670, [32, 128])
+        self.bot = Bot((1920 - 200), 620, [32, 128])
 
-        self.tour_joueur = True  # Le joueur commence
-        self.en_attente = False  # Attente entre les tours
-        self.temps_attente = 0  # Temps de début d'attente
-        self.explosion_active = False  # True si une explosion est affichée
+        self.tour_joueur = True
+        self.en_attente = False
+        self.temps_attente = 0
+        self.explosion_active = False
 
         self.trajectoire = Trajectoire()
         self.masse_projectile = self.get_masse_projectile()
+
+        self.debut_partie = pygame.time.get_ticks()
 
     def charger_donnees_json(self, fichier):
         with open(fichier, "r", encoding="utf-8") as f:
@@ -56,18 +59,22 @@ class Jeu:
         return pygame.image.load("assets/images/armes/default_projectile.png").convert_alpha()
 
     def gerer_evenements_jeu(self, event):
+        temps_ecoule_depuis_debut = pygame.time.get_ticks() - self.debut_partie
+        if temps_ecoule_depuis_debut < 1000:
+            return
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.tour_joueur:
             self.trajectoire.start_charging()
 
-        if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.tour_joueur and mon_bouton_parametre.action and self.joueur.angle <= 95 and self.bot.pv>0 and self.joueur.pv>0:
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.tour_joueur and mon_bouton_parametre.action and self.joueur.angle <= 95 and self.bot.pv > 0 and self.joueur.pv > 0:
             vitesse_initiale = self.trajectoire.stop_charging_and_compute_speed(self.masse_projectile)
-            puissance = vitesse_initiale / 10  # Exemple de conversion
+            puissance = vitesse_initiale / 10
             x_proj, y_proj = self.joueur.position_depart_projectile()
-            projectile = Projectile(x_proj, y_proj, [60, 60], self.image_projectile, self.joueur.angle,
+            projectile = Projectile(x_proj, y_proj, [60, 60], self.image_projectile_joueur, self.joueur.angle,
                                     puissance, vitesse_initiale, "joueur")
             self.projectiles_joueur.add(projectile)
             self.piece.monnaie_joueur += 1
-            print(f"Le projectile a été tiré : puissance={puissance:.2f}, vitesse_initiale={vitesse_initiale:.2f}")  # Debug
+            print(f"Le projectile a été tiré : puissance={puissance:.2f}, vitesse_initiale={vitesse_initiale:.2f}")
             self.temps_attente = pygame.time.get_ticks()
             self.en_attente = True
             self.tour_joueur = False
@@ -79,16 +86,16 @@ class Jeu:
             if pygame.time.get_ticks() - self.temps_attente >= 3000 and not self.explosion_active:
                 if not self.projectiles_joueur and not self.projectiles_bot:
                     self.en_attente = False
-                    if not self.tour_joueur and self.bot.pv>0 and self.joueur.pv>0:
+                    if not self.tour_joueur and self.bot.pv > 0 and self.joueur.pv > 0:
                         angle, puissance = self.bot.tir(self.joueur.rect.centerx, self.joueur.rect.centery)
-                        vitesse_initiale = 110 + (7 * puissance)  # Vous pouvez ajuster cette valeur selon vos besoins
+                        vitesse_initiale = 110 + (7 * puissance)
                         projectile = Projectile(self.bot.rect.centerx, self.bot.rect.centery, [60, 60],
-                                                self.image_projectile, angle, puissance, vitesse_initiale, "bot")
+                                                self.image_projectile_bot, angle, puissance, vitesse_initiale, "bot")
                         self.projectiles_bot.add(projectile)
                         self.temps_attente = pygame.time.get_ticks()
                         self.en_attente = True
                         self.tour_joueur = True
-                        print(f"Le bot a tiré.")  # Debug
+                        print(f"Le bot a tiré.")
 
         for projectile in self.projectiles_joueur:
             projectile.mouvement(self.bot, self.piece, self, self.sons)
@@ -128,14 +135,12 @@ class Jeu:
         while continuer:
             pos_souris = pygame.mouse.get_pos()
 
-            # ----- 1. GESTION DES ÉVÉNEMENTS -----
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     continuer = False
                 self.gerer_evenements_jeu(event)
-            # ----- 2. MISE À JOUR LOGIQUE (appelée à chaque frame) -----
+
             self.mettre_a_jour_jeu()
-            # ----- 3. AFFICHAGE -----
             self.afficher_jeu(pos_souris)
             pygame.display.update()
             clock.tick(40)
