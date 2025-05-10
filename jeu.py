62d04b7d214cff34c5bfbcc7d4fe9224cbc18357
Jaspe35 <flavie.bremand@efrei.net>
2025-05-10 19:10:25 +0200

    Commentaires bot.py

--- jeu.py
+++ jeu.py
@@ -106,7 +106,7 @@ class Jeu:
 
     def gerer_evenements_jeu(self, event):
         temps_ecoule_depuis_debut = pygame.time.get_ticks() - self.debut_partie
-        if temps_ecoule_depuis_debut < 100:
+        if temps_ecoule_depuis_debut < 500:
             return
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.tour_joueur:
